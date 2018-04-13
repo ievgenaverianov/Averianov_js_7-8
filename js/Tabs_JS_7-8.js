ade74a4c86f204d4f6a51dc1ad122a358902fc55
Ievgen Averianov <averyanovsumy@gmail.com>
2018-04-14 01:57:01 +0300

    refactoring

--- js/Tabs_JS_7-8.js
+++ js/Tabs_JS_7-8.js
@@ -1,43 +1,38 @@
 $(function () {
 
-  $('body').append('<div class="tabsContainer"></div>');
-  var $oTabsContainerElem = $('.tabsContainer');
-  $oTabsContainerElem.append('<ul class="tabsMenu"></ul>');
+  var $oTabsContainerElem = $('<div class="tabsContainer"></div>');
+  $('body').append($oTabsContainerElem);
 
-  var $oTabsMenuElem = $('.tabsMenu');
-  $oTabsMenuElem.append('<li class="tabsMenuItem tabsMenuItemActive" id="tabsMenuItem_1">Bruce Lee</li>');
-  $oTabsMenuElem.append('<li class="tabsMenuItem" id="tabsMenuItem_2">Goethe</li>');
-  $oTabsMenuElem.append('<li class="tabsMenuItem" id="tabsMenuItem_3">Marc Andreessen </li>');
+  var $oTabsMenuElem = $('<ul class="tabsMenu"></ul>');
+  $oTabsContainerElem.append($oTabsMenuElem);
+
+  var $oTabsMenuItemElem1 = $('<li class="tabsMenuItem tabsMenuItemActive" id="tabsMenuItem_1">Bruce Lee</li>');
+  var $oTabsMenuItemElem2 = $('<li class="tabsMenuItem" id="tabsMenuItem_2">Goethe</li>');
+  var $oTabsMenuItemElem3 = $('<li class="tabsMenuItem" id="tabsMenuItem_3">Marc Andreessen </li>');
+  $oTabsMenuElem.append($oTabsMenuItemElem1, $oTabsMenuItemElem2, $oTabsMenuItemElem3);
 
   var $oTabsMenuItemElem = $('.tabsMenuItem');
   $oTabsMenuItemElem.attr('tabindex', '0');
 
-  var $oTabsMenuItemElem1 = $('#tabsMenuItem_1');
-  var $oTabsMenuItemElem2 = $('#tabsMenuItem_2');
-  var $oTabsMenuItemElem3 = $('#tabsMenuItem_3');
+  var $oTabsMenuItemContent1 = $('<div class="tabsMenuItemContent" id="tabsMenuItemContent1"><p>Because you might as ' +
+    'well be dead. Seriously, if you always put limits on what you can do, physical or anything else, it’ll spread ' +
+    'over into the rest of your life. It’ll spread into your work, into your morality, into your entire being. There ' +
+    'are no limits. There are plateaus, but you must not stay there, you must go beyond them. If it kills you, it ' +
+    'kills you. A man must constantly exceed his level.</p></div>');
+  var $oTabsMenuItemContent2 = $('<div class="tabsMenuItemContent" id="tabsMenuItemContent2"><p>Daring ideas are like ' +
+    'chessmen moved forward; they may be beaten, but they may start a winning game.</p></div>');
+  var $oTabsMenuItemContent3 = $('<div class="tabsMenuItemContent" id="tabsMenuItemContent3"><p>Curious people are ' +
+    'more likely to already have in their heads the building blocks for crafting a solution for any particular problem ' +
+    'they come across, versus the more quote-unquote intelligent, but less curious, person who is trying to get by on ' +
+    'logic and pure intellectual effort.</p></div>');
+  $oTabsContainerElem.append($oTabsMenuItemContent1, $oTabsMenuItemContent2, $oTabsMenuItemContent3);
 
-  $oTabsContainerElem.append('<div class="tabsMenuItemContent" id="tabsMenuItemContent1"><p>Because you might as well be dead. ' +
-    'Seriously, if you always put limits on what you can do, physical or anything else, it’ll spread over into the rest' +
-    ' of your life. It’ll spread into your work, into your morality, into your entire being. There are no limits. There ' +
-    'are plateaus, but you must not stay there, you must go beyond them. If it kills you, it kills you. A man must ' +
-    'constantly exceed his level.</p></div>');
-  $oTabsContainerElem.append('<div class="tabsMenuItemContent" id="tabsMenuItemContent2"><p>Daring ideas are like chessmen moved ' +
-    'forward; they may be beaten, but they may start a winning game.</p></div>');
-  $oTabsContainerElem.append('<div class="tabsMenuItemContent" id="tabsMenuItemContent3"><p>Curious people are more likely to ' +
-    'already have in their heads the building blocks for crafting a solution for any particular problem they come ' +
-    'across, versus the more quote-unquote intelligent, but less curious, person who is trying to get by on logic ' +
-    'and pure intellectual effort.</p></div>');
-
-
-  var $oTabsMenuItemContent1 = $('#tabsMenuItemContent1');
   $oTabsMenuItemContent1.css({
     display: 'block'
   });
-  var $oTabsMenuItemContent2 = $('#tabsMenuItemContent2');
   $oTabsMenuItemContent2.css({
     display: 'none'
   });
-  var $oTabsMenuItemContent3 = $('#tabsMenuItemContent3');
   $oTabsMenuItemContent3.css({
     display: 'none'
   });
